01d7f997cf798eb8c5a14453d65ac31fc3a97ccc
ashrafedes <ashrafedes@gmail.com>
2026-08-11 21:38:40 +0300

    Switch to HashRouter for Render static site

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from '@/contexts/AuthContext'
+import { LanguageProvider } from '@/contexts/LanguageContext'
 import App from '@/App'
+import '@/i18n' // side-effect: initializes i18next
 import '@/index.css'
 import 'react-data-grid/lib/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <LanguageProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </LanguageProvider>
+    </HashRouter>
   </React.StrictMode>,
 )
